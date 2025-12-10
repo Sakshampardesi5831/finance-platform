353c1f9b2288b5a9b03eb726b6c8ccc1f3286af7
Saksham Pardesi <sakshampardesi5831@gmail.com>
2025-12-10 15:07:30 +0530

    transactions api and hooks complete

---
+++ features/transactions/api/use-delete-transaction.ts
@@ -0,0 +1,27 @@
+import { toast } from "sonner";
+import { InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<typeof client.api.transactions[":id"]["$delete"]>;
+
+export const useDeleteTransaction = (id?: string) => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.transactions[":id"].$delete({
+        param: { id },
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("transaction deleted");
+      queryClient.invalidateQueries({ queryKey: ["transaction", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+    },
+    onError: () => {
+      toast.error("failed to delete transaction");
+    },
+  });
+  return mutation;
+};
